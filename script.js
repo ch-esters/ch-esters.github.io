c87e792419609153739cf85306d430994997ffaa
ella <ella3153@gmail.com>
2020-07-01 00:02:33 +0300

    commit!

---
+++ script.js
@@ -0,0 +1,99 @@
+function rum()
+{
+	var win = window.open("https://mega.nz/file/nNRSQCaC#pmoROIOmePVL1Stad29R45WdbeLFpJHBR2rrw1GQoZ0", '_blank');
+    win.focus();
+}
+
+function vibes()
+{
+	var win = window.open("https://mega.nz/file/eEAwBACQ#c37_NctEvnrRJiNewhnE9jAMUdIR5ZaRuPPvJG6fEsw", '_blank');
+    win.focus();
+}
+
+function paper()
+{
+	var win = window.open("https://mega.nz/file/ucAUmQqZ#MRnxcN1OaZWOGnbyTGf2s-KDnV2cSnthiq3skIuFfi8", '_blank');
+    win.focus();
+}
+
+function shutup()
+{
+	var win = window.open("https://mega.nz/file/CFZywIQL#1yZHLQCT5DgoS1QQOvylyjIPTsqbXYsSRcpYDWf_VAA", '_blank');
+    win.focus();
+}
+
+function crazy()
+{
+	var win = window.open("https://mega.nz/file/CUAyXQqY#QFWQTDn7s-15tiVsvFdoc7KK9M9CT4uTBJxPZAgo1Xg", '_blank');
+    win.focus();
+}
+
+function dancing()
+{
+	var win = window.open("https://mega.nz/file/mMZ00KhS#l-QnNHiAErfy3KINzMJ8qvsFJ2VVYHCfPUbJNQhjKbo", '_blank');
+    win.focus();
+}
+
+
+function tightrope()
+{
+	var win = window.open("https://mega.nz/file/GFQ2yYJB#mZkbaFlvdbBvh2K8UkGDd1omJArVA6VPS1QBDgXZs_s", '_blank');
+    win.focus();
+}
+
+
+function iris()
+{
+	var win = window.open("https://mega.nz/file/qIR0EKTK#HsursWZJ7gYXsFQK2u6Pilv6WEkPEn6a4jm7KxURF7o", '_blank');
+    win.focus();
+}
+
+
+function twenty()
+{
+	var win = window.open("https://mega.nz/file/nJRg2AgT#hB0wBUpNwZ2fodwcOjW8ha6R8SQws6fIx2ZVdsBeueQ", '_blank');
+    win.focus();
+}
+
+
+function empty()
+{
+	var win = window.open("https://mega.nz/file/7UZmQCja#jH88NqepBzcRBzvf32xJDxZyDG78v4sg5Qekbt4gVwU", '_blank');
+    win.focus();
+}
+
+function drugs()
+{
+	var win = window.open("https://mega.nz/file/nYJgECbY#SxAxc59lhkWcttPkde3Ng55jajP1vTNAnH17lJsIOSM", '_blank');
+    win.focus();
+}
+
+function habit()
+{
+	var win = window.open("https://mega.nz/file/jdQ23KZA#lzxSOCoS0hUr9KnN4daLAPAL0492PDH8wGx0xSEmLOU", '_blank');
+    win.focus();
+}
+
+function summer()
+{
+	var win = window.open("https://mega.nz/file/DIRQ0IAQ#9pHAk8T5pB2ifQ-D5FJENEBTyVZiIZaa7AHT7HxRRmQ", '_blank');
+    win.focus();
+}
+
+function double()
+{
+	var win = window.open("https://mega.nz/file/bFYECC7A#zd-80yOpjrcodk3TaiI452qabvSYNBkOEQF6uGH0dNA", '_blank');
+    win.focus();
+}
+
+function goodie()
+{
+	var win = window.open("https://mega.nz/file/uEZG3KJA#w54oPsEYb2LLL8EUxERTZpHoNQfVVnQGmN2_BdDGrXs", '_blank');
+    win.focus();
+}
+
+function mega()
+{
+	var win = window.open("https://mega.nz/folder/aRI2QQAD#NA_uugN7JK4QlFX0U_91Ew", '_blank');
+    win.focus();
+}
